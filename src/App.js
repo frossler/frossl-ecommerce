2d3abe39ef1cd9778602cfd6a3df5dc66425811d
frossler <frossler89@gmail.com>
2021-12-07 01:30:27 -0300

    ItemListContainer fixes

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Components:
 import Header from './components/header/Header';
-
+import ItemListContainer from './components/itemlistcontainer/ItemListContainer';
 
 
 function App() {
   return (
         <>
         <Header />
-        
+        <ItemListContainer id="offers" />
         </>
         
 
---
+++ src/components/itemlistcontainer/ItemListContainer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ItemListContainer = (props) => {
+
+    console.log(props)
+
+    return (
+        <p>Showing all the {props.id} !</p>
+    )
+}
+
+export default ItemListContainer
