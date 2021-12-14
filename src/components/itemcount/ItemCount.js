1668f0cf15b35a7d1e4b62663b1fc54984788a73
frossler <frossler89@gmail.com>
2021-12-14 20:26:55 -0300

    Changes

--- src/components/itemcount/ItemCount.js
+++ src/components/itemcount/ItemCount.js
@@ -3,35 +3,32 @@ import { useState } from "react";
 import { Button } from "react-bootstrap";
 import toast, { Toaster } from 'react-hot-toast';
 
-const ItemCount = (props) => {
+const ItemCount = (props, onAdd) => {
 
     // Stock management 
     const stock = props.count
     const [counter, setCounter] = useState(1)
     const add = () => { 
-        if (counter < stock) { setCounter(counter+1) } else {toast.error('No More Stock for Adding...', {duration: 2000})}
+        if (counter < stock) { setCounter(counter+1) } 
+        else {
+          toast.error('No More Stock for Adding...', {duration: 2000})
+        }
       }
 
     const rem = () => {
         if (counter > 1) { setCounter(counter - 1) } 
-        else {
+        else { 
           toast.error('Should be at least 1 to Add...', {duration: 2000})
         }
       }
-
-    
-    // onAdd to Cart
-    const onAdd = () => {
-      return toast.success(counter + 'Products Added to cart.' , {duration: 2000})
       
-    }   
-
     return(
         <>
-            <Button variant="outline-info" onClick={rem}> - </Button>{' '}
-            <Button variant="outline-primary" onClick={onAdd}> onAdd: {counter} </Button>{' '}
+        <Toaster/>
+            <Button variant="outline-info" onClick={rem}> - </Button>{''}
+            <Button variant="outline-primary" onClick={onAdd}> onAdd=Add to Cart Button: {counter} </Button>{' '}
             <Button variant="outline-info" onClick={add}> + </Button>{''}
-            <Toaster/>
+            
         </>
 
     )
--- src/components/itemlistcontainer/ItemListContainer.js
+++ src/components/itemlistcontainer/ItemListContainer.js
@@ -1,32 +1,26 @@
 import React from "react";
 // import { useState, useEffect } from "react"
-import ItemCount from "../itemcount/ItemCount";
+import ItemCount from "../itemcount/ItemCount"
+import toast, { Toaster } from 'react-hot-toast'
 
 const ItemListContainer = (props) => {
 
     //
     console.log(props)
-    // Effect delays and run after Component Render
 
-    // useEffect (()=>{
-    //     console.log("Intervalo Ping 1000")
-    //     // setTimeout(()=>[])
-    //     // Se setea un indicador
-    //     const i = setInterval(()=>{
-    //         console.log("Ping foo")
-    //     },1000)
-    
-    //     return () =>{
-    //         console.log("Cleanup foo")
-    //         clearInterval(i) //pasar el indicador
-    //     }
-    // })
-
+    //onAdd to Cart
+    function onAdd(){
+      return (
+          toast.success('Products Added to cart.' , {duration: 2000})
+          
+      )
+    }   
 
     return (
         <>
+        <Toaster/>
         <p>Showing all the {props.id} !</p>
-        <ItemCount count="3"/>
+        <ItemCount count="3" onAdd={onAdd} initial="1"/>
         </>
     )
 }
