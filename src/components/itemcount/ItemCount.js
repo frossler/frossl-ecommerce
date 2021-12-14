30bc14fe6794673e3305aae25a4d644a7ed04739
frossler <frossler89@gmail.com>
2021-12-14 12:37:40 -0300

    Toast added replacing alerts

--- src/components/itemcount/ItemCount.js
+++ src/components/itemcount/ItemCount.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { Button } from "react-bootstrap";
+import toast, { Toaster } from 'react-hot-toast';
 
 const ItemCount = (props) => {
 
@@ -8,20 +9,21 @@ const ItemCount = (props) => {
     const stock = props.count
     const [counter, setCounter] = useState(1)
     const add = () => { 
-        if (counter < stock) {setCounter(counter+1)} else {alert("Out of Stock for adding")}
+        if (counter < stock) { setCounter(counter+1) } else {toast.error('No More Stock for Adding...', {duration: 2000})}
       }
+
     const rem = () => {
-        if (counter > 1) {
-          setCounter(counter - 1);
-        } else {
-          alert("Should be at least 1 to Add...");
+        if (counter > 1) { setCounter(counter - 1) } 
+        else {
+          toast.error('Should be at least 1 to Add...', {duration: 2000})
         }
-      };
+      }
 
     
     // onAdd to Cart
     const onAdd = () => {
-      return alert( counter + " Product/s Added to Cart")
+      return toast.success(counter + 'Products Added to cart.' , {duration: 2000})
+      
     }   
 
     return(
@@ -29,6 +31,7 @@ const ItemCount = (props) => {
             <Button variant="outline-info" onClick={rem}> - </Button>{' '}
             <Button variant="outline-primary" onClick={onAdd}> onAdd: {counter} </Button>{' '}
             <Button variant="outline-info" onClick={add}> + </Button>{''}
+            <Toaster/>
         </>
 
     )
